847d5bfa64865fc8c0cdc605ec59f838a48676f8
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 21:14:50 +0200

    Contact class complete

--- ManagementCompany/Contact.cs
+++ ManagementCompany/Contact.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ManagementCompany
 {
+    /// <summary>
+    /// Management company save contacts on workers
+    /// </summary>
     internal class Contact
     {
+        /// <summary>
+        /// If we need worker often, thats contact could be favorite.
+        /// </summary>
+        private bool favorite { get; set; }
+        public Contact(string name, string surname, uint telephone, Profession profesion, bool favorite) : base(name, surname, telephone, profesion)
+        {
+            this.favorite = favorite;
+        }
+
+        /// <summary>
+        /// Via mobile phone we call worker to repair something
+        /// </summary>
+        /// <param name="flat"></param>
+        /// <param name="issue"></param>
+        /// <returns></returns>
+        public string CallWorkerToWork(Flat flat, string issue) => Repair(flat, issue);
     }
 }
