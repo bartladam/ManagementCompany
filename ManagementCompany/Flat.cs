7ecaaa09880c0aa3aff2d1af1bebb1cd34e9e1a5
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 21:18:52 +0200

    Flat class complete

--- ManagementCompany/Flat.cs
+++ ManagementCompany/Flat.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace ManagementCompany
 {
+    /// <summary>
+    /// Flat is management by management company
+    /// </summary>
     internal class Flat
     {
+        /// <summary>
+        /// Flat has address for worker. Worker need know where flat is.
+        /// </summary>
+        public string Adress { get; private set; }
+        /// <summary>
+        /// Flat in during his lifetime is need some repair
+        /// </summary>
+        public List<string> problems { get; set; }
+        public Flat(string adress)
+        {
+            this.Adress = adress;
+        }
+        /// <summary>
+        /// Owner can add new problems
+        /// </summary>
+        /// <param name="problems"></param>
+        public void newProblem(params string[] problems)
+        {
+            this.problems = new List<string>(problems);
+        }
     }
 }
