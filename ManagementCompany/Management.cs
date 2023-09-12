660a602a147f23e482340b8a94d49d4d6c5f794b
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 21:13:12 +0200

    Management complete

--- ManagementCompany/Management.cs
+++ ManagementCompany/Management.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace ManagementCompany
 {
+    /// <summary>
+    /// Management company solve problems owners
+    /// </summary>
     internal class Management
     {
+        /// <summary>
+        /// Management company manage a lot of flats. Not only one flat
+        /// </summary>
+        public List<Flat> flats { get; private set; }
+        /// <summary>
+        /// Management company have to contacts on workers 
+        /// </summary>
+        public List<Contact> contacts { get; private set; }
+        public Management(params Flat[] flats)
+        {
+            this.flats = new List<Flat>(flats);
+            contacts = new List<Contact>();
+        }
+        /// <summary>
+        /// Adding new contact on workers
+        /// </summary>
+        /// <param name="contact"></param>
+        public void newContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+        /// <summary>
+        /// Company is solving problems owner flats
+        /// </summary>
+        /// <returns></returns>
+        public string Solution()
+        {
+            string solveIssue = "";
+            foreach (Flat item in flats)
+            {
+                List<string> issues = new List<string>(item.problems);
+                foreach (string item2 in issues)
+                {
+                    foreach (Contact contactWorker in contacts)
+                    {
+                        if (item2.Equals(contactWorker.profesion.ToString()))
+                            solveIssue += contactWorker.CallWorkerToWork(item, item2) + "\n";
+
+                    }
+                }
+
+            }
+            return solveIssue;
+        }
     }
 }
